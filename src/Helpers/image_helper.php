20a3fb15118270721497914eb4539b03899d7405
Joas Sanon <msejoas@gmail.com>
2026-06-07 06:57:56 -0400

    update package.json and image_helper.php

--- src/Helpers/image_helper.php
+++ src/Helpers/image_helper.php
@@ -15,7 +15,7 @@ if (! function_exists('optimized_image_urls')) {
     function optimized_image_urls(string $src, array $widths = null, $quality = 75, $format = null)
     {
         $widths ??= [320,480,768,1024,1366,1600,1920];
-        $base = url('/img');
+        $base = URL::to('/img');
         $sources = [];
         foreach ($widths as $w) {
             $query = http_build_query(['src' => $src, 'w' => $w, 'q' => $quality, 'fm' => $format]);
